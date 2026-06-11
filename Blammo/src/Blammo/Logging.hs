2e8ab4429b91d914826d091854adc8f17bb6fd1d
patrick brisbin <pbrisbin@gmail.com>
2026-06-11 09:32:52 -0400

    Add logTrace functions
    These are named functions logging with `LevelOther "trace"`, which is a
    log-level used by many libraries as a level more verbose than `debug`,
    for example Amazonka.
    
    We already have special support for `trace` when filtering by
    log-levels, to ensure that trace messages are considered "less than"
    debug, so silencing works as expected. By default, "other" log levels
    are considered "greater than" all non-other levels, so without this
    trace messages would appear even at `LOG_LEVEL=error`.
    
    Given that, it seems reasonable to consider it a 1st-class notion in
    Blammo, and include `logTrace` functions too.
    
    Closes #48.

--- Blammo/src/Blammo/Logging.hs
+++ Blammo/src/Blammo/Logging.hs
@@ -18,6 +18,7 @@ module Blammo.Logging
     -- | Import "Control.Monad.Logger.Aeson" if you want more
 
     -- *** Implicit call stack, no 'LogSource'
+  , logTrace
   , logDebug
   , logInfo
   , logWarn
@@ -26,6 +27,7 @@ module Blammo.Logging
 
     -- *** Implicit call stack, with 'LogSource'
   , LogSource
+  , logTraceNS
   , logDebugNS
   , logInfoNS
   , logWarnNS
@@ -33,6 +35,14 @@ module Blammo.Logging
   , logOtherNS
   ) where
 
+import Prelude
+
 import Blammo.Logging.Logger
 import Control.Monad.Logger.Aeson
 import Data.Aeson (Series)
+
+logTrace :: MonadLogger m => Message -> m ()
+logTrace = logOther $ LevelOther "trace"
+
+logTraceNS :: MonadLogger m => LogSource -> Message -> m ()
+logTraceNS s = logOtherNS s $ LevelOther "trace"
